6dfe9f76a075bc7d11e5190db88cb33d0c7e80a5
David O'Mullan <35239651+davidomullan@users.noreply.github.com>
2023-09-09 16:46:56 -0500

    Fix syntax to meet ruby style guide

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,24 +1,26 @@
-# bubble_sort
-# 	Description: Returns the user provided array sorted numerically following
-# 							 the bubble sort methodology.
-# 	parameters: Array of integers (no limits)
-# 	return value: Array of integers (sorted numerically)
-def bubble_sort(array)
-	puts array.join()
-	array.each_with_index { |element, arrayIndex|
-		if arrayIndex!=array.length-1
-			if element>array[arrayIndex+1]
-				temp = element
-			 	array[arrayIndex]=array[arrayIndex+1]
-			 	array[arrayIndex+1] = temp
-			end
-		end
-		puts array.join()
-	}
-	if array.length>2
-		lastElem = array[array.length-1]
-		array = bubble_sort(array[0..array.length-2]).push(lastElem)
-	end
+# frozen_string_literal: true
 
-	return array
+# bubble_sort
+#   Description: Returns the user provided array sorted numerically following
+#                the bubble sort methodology.
+#   parameters: Array of integers (no limits)
+#   return value: Array of integers (sorted numerically)
+
+def bubble_sort(array)
+  array = find_position(array)
+  array = bubble_sort(array[0..array.length - 2]).push(array[array.length - 1]) if array.length > 2
+  array
+end
+
+private
+
+def find_position(array)
+  array.each_with_index do |element, array_index|
+    next unless array_index != array.length - 1 && element > array[array_index + 1]
+
+    temp = element
+    array[array_index] = array[array_index + 1]
+    array[array_index + 1] = temp
+  end
+  array
 end
